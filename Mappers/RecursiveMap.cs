c70eea8c24116e06d6cd2e2b6c834d2801d6b288
Harry Miller <Harry.Miller@connellsgroup.co.uk>
2024-10-04 15:53:51 +0100

    Added support for enum properties

--- Mappers/RecursiveMap.cs
+++ Mappers/RecursiveMap.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Reflection;
 using ObjectMapper.Extensions;
 using ObjectMapper.Mappers.Interfaces;
 
@@ -46,51 +47,24 @@ namespace ObjectMapper.Mappers
                 {
                     var sourceValue = sourceProperty.GetValue(source);
 
+                    if (property.PropertyType.IsEnum)
+                    {
+                        var convertedEnum = Enum.ToObject(property.PropertyType, sourceValue!);
+                        property.SetProperty(convertedEnum!, destination!);
+                        continue;
+                    }
+
                     if (property.IsClass())
                     {
                         var populatedProperty = Map(sourceValue!, property.PropertyType);
                         property.SetProperty(populatedProperty!, destination!);
-
                         continue;
                     }
 
                     if (property.IsDictionary())
                     {
-                        var sourceDictionary = sourceValue as IDictionary;
-                        var targetDictionary = new Dictionary<object, object>();
-
-                        // Get the target key and value types to create the key value pair for
-                        var keyType = property.PropertyType.GenericTypeArguments[0];
-                        var valueType = property.PropertyType.GenericTypeArguments[1];
-
-                        foreach (DictionaryEntry pair in sourceDictionary!)
-                        {
-                            object key = null!;
-                            object value = null!;
-
-                            if (keyType.IsSystemType())
-                            {
-                                key = Convert.ChangeType(pair.Key, keyType);
-                            }
-                            else
-                            {
-                                key = Map(pair.Key!, keyType);
-                            }
-
-                            if (valueType.IsSystemType())
-                            {
-                                value = Convert.ChangeType(pair.Value, valueType)!;
-                            }
-                            else
-                            {
-                                value = Map(pair.Value!, valueType);
-                            }
-
-                            targetDictionary.Add(key, value);
-                        }
-
-                        property.SetEnumerableProperty(targetDictionary.ToTypedDictionary(keyType, valueType), destination!);
-
+                        var targetDictionary = MapDictionaryProperty(sourceValue!, property);
+                        property.SetEnumerableProperty(targetDictionary, destination!);
                         continue;
                     }
 
@@ -98,37 +72,9 @@ namespace ObjectMapper.Mappers
                     {
                         // Check wherever the IEnumerable is an array or IEnumerable<T>
                         var isArray = sourceProperty.PropertyType.IsArray;
-
-                        var sourceEnumerable = sourceValue as IEnumerable;
-                        var sourceEnumerableValue = sourceEnumerable!.Cast<object>();
-
-                        var length = sourceEnumerableValue?.Count() ?? 0;
-
-                        object valueToSet = null!;
-                        var elementType = isArray ? property.PropertyType.GetElementType() : property.PropertyType.GenericTypeArguments[0];
-
-                        // Check if the element type is a class
-                        var isClass = elementType!.IsClass();
-
-                        if (isClass)
-                        {
-                            var targetEnumerable = new List<object>(length);
-
-                            foreach (var element in sourceEnumerableValue!)
-                            {
-                                var populatedElement = Map(element!, elementType!);
-                                targetEnumerable.Add(populatedElement);
-                            }
-
-                            valueToSet = CreateTypedEnumerableOrArray(targetEnumerable, elementType!, isArray);
-                        }
-                        else
-                        {
-                            valueToSet = CreateTypedEnumerableOrArray(sourceEnumerableValue!, elementType!, isArray);
-                        }
+                        var valueToSet = MapEnumerableProperty(sourceValue!, property, isArray);
 
                         property.SetEnumerableProperty(valueToSet!, destination!);
-
                         continue;
                     }
 
@@ -142,6 +88,77 @@ namespace ObjectMapper.Mappers
         private object CreateTypedEnumerableOrArray(IEnumerable<object> source, Type elementType, bool isArray)
         {
             return isArray ? source!.ToTypedArray(elementType!) : source!.ToTypedEnumerable(elementType!);
+        }
+
+        private object MapEnumerableProperty(object sourceValue, PropertyInfo property, bool isArray)
+        {
+            var sourceEnumerable = sourceValue as IEnumerable;
+            var sourceEnumerableValue = sourceEnumerable!.Cast<object>();
+
+            var length = sourceEnumerableValue?.Count() ?? 0;
+
+            object valueToSet = null!;
+            var elementType = isArray ? property.PropertyType.GetElementType() : property.PropertyType.GenericTypeArguments[0];
+
+            // Check if the element type is a class
+            var isClass = elementType!.IsClass();
+
+            if (isClass)
+            {
+                var targetEnumerable = new List<object>(length);
+
+                foreach (var element in sourceEnumerableValue!)
+                {
+                    var populatedElement = Map(element!, elementType!);
+                    targetEnumerable.Add(populatedElement);
+                }
+
+                valueToSet = CreateTypedEnumerableOrArray(targetEnumerable, elementType!, isArray);
+            }
+            else
+            {
+                valueToSet = CreateTypedEnumerableOrArray(sourceEnumerableValue!, elementType!, isArray);
+            }
+
+            return valueToSet;
+        }
+
+        private object MapDictionaryProperty(object sourceValue, PropertyInfo property)
+        {
+            var sourceDictionary = sourceValue as IDictionary;
+            var targetDictionary = new Dictionary<object, object>();
+
+            // Get the target key and value types to create the key value pair for
+            var keyType = property.PropertyType.GenericTypeArguments[0];
+            var valueType = property.PropertyType.GenericTypeArguments[1];
+
+            foreach (DictionaryEntry pair in sourceDictionary!)
+            {
+                object key = null!;
+                object value = null!;
+
+                if (keyType.IsSystemType())
+                {
+                    key = Convert.ChangeType(pair.Key, keyType);
+                }
+                else
+                {
+                    key = Map(pair.Key!, keyType);
+                }
+
+                if (valueType.IsSystemType())
+                {
+                    value = Convert.ChangeType(pair.Value, valueType)!;
+                }
+                else
+                {
+                    value = Map(pair.Value!, valueType);
+                }
+
+                targetDictionary.Add(key, value);
+            }
+
+            return targetDictionary.ToTypedDictionary(keyType, valueType);
         }
     }
 }
